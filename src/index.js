a13b5516ef9b06c2a11712347e3ec7b4f5ddd187
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-07 11:36:12 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './globalStyle';
+import { theme } from './theme';
+import Normalize from 'react-normalize';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    white: "#FFFFFF",
+    blue: "#4FA4EC",
+    crimson: "#DC143C",
+  },
+  breakpoints: {
+    mobile: "767px",
+  },
+};

b1f18266c6c1c7ce4bbe6081e59b4aaea8be2e61
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-08 10:58:48 +0200

    added color and breakpoint

--- src/theme.js
+++ src/theme.js
@@ -3,10 +3,12 @@ export const theme = {
     white: "#FFFFFF",
     gray: "#7D7D7D",
     celadonBlue: "#0E77AF",
+    celadonBlueOpacity:"#0e77afed",
     darkTangerine: "#FBA518",
   },
   breakpoints: {
     mobile: "767px",
+    desktops: "992px",
     tabletLandscape: "1024px",
   },
 };

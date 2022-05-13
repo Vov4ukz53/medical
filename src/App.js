0c10f27049179d008a2634eb3faa690b583e3d68
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-13 21:16:28 +0200

    added component About

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Header } from "./features/header";
 import { SliderSection } from "./features/sliderSection";
 import { Footer } from "./features/footer";
+import { About } from "./features/about";
 
 function App() {
   return (
     <>
       <Header />
       <SliderSection />
+      <About />
       <Footer />
     </>
   )

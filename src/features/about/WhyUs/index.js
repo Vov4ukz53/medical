7ec42ab2df7931cab5d804e3c2328f97a350ffe8
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-29 22:46:37 +0200

    rename block to readMoreBlock

--- src/features/about/WhyUs/index.js
+++ src/features/about/WhyUs/index.js
@@ -1,5 +1,5 @@
 import { Title } from "../../../common/Title";
-import { Block } from "./Block";
+import { ReadMoreBlock } from "../../../common/ReadMoreBlock";
 import { Content } from "./styled";
 import { ReactComponent as Icon1 } from "./icons/icon1.svg";
 import { ReactComponent as Icon2 } from "./icons/icon2.svg";
@@ -9,19 +9,19 @@ export const WhyUs = () => (
   <div>
     <Title>Why Us?</Title>
     <Content>
-      <Block
+      <ReadMoreBlock whyUs
         title="Dependable"
         icon={<Icon1 />}
         text="Our skilled and trustworthy teams ensure process continuity and maintain high standards of quality"
         textMore=" and trustworthy teams ensure process continuity."
       />
-      <Block next
+      <ReadMoreBlock next whyUs
         title={"Open"}
         icon={<Icon2 />}
         text="You are assured of transparency and openness in our staffing, workflows and communication protocol."
         textMore=" You are assured of transparency and openness in our staffing."
       />
-      <Block
+      <ReadMoreBlock whyUs
         title="Connected"
         icon={<Icon3 />}
         text="There is never a time when some one will not be in touch with you! There is never a time"

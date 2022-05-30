bdb2dafa02146d8046325b8d08fd3c63ad2c6766
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-30 21:27:59 +0200

    added media query to subTitle

--- src/common/SubTitle.js
+++ src/common/SubTitle.js
@@ -6,7 +6,7 @@ export const SubTitle = styled.h3`
   color: ${({ theme }) => theme.colors.celadonBlue};
   margin: 0px;
 
-  @media(max-width: ${({theme})=> theme.breakpoints.desktops}){
+  @media(max-width: ${({ theme }) => theme.breakpoints.desktops}){
     font-size: 20px;
   }
 `;

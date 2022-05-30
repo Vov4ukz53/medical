715744bb7415b70f343a0ab79aad9934f4986303
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-30 21:29:22 +0200

    added section and rename wrapper to body

--- src/features/about/index.js
+++ src/features/about/index.js
@@ -1,13 +1,15 @@
 import { Container } from "../../common/Container";
 import { AboutUs } from "./AboutUs";
-import { Wrapper } from "./styled";
+import { Body } from "./styled";
 import { WhyUs } from "./WhyUs";
 
 export const About = () => (
-  <Container>
-    <Wrapper>
-      <AboutUs />
-      <WhyUs />
-    </Wrapper>
-  </Container>
+  <section>
+    <Container>
+      <Body>
+        <AboutUs />
+        <WhyUs />
+      </Body>
+    </Container>
+  </section>
 );
--- src/features/about/styled.js
+++ src/features/about/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.section`
+export const Body = styled.div`
   display: grid;
   grid-template-columns: 65.5% 1fr;
   grid-gap: 50px;

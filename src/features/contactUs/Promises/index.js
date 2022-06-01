ff7e6f30c702277261d9a412de70aea24cd1ffdd
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-06-01 22:44:23 +0200

    added promises column

---
+++ src/features/contactUs/Promises/index.js
@@ -0,0 +1,44 @@
+import { ReadMoreBlock } from "../../../common/ReadMoreBlock";
+import { TitleBlock } from "../../../common/TitlleBlock";
+import { Wrapper, Item, Paragraph } from "./styled";
+
+export const Promises = () => (
+  <Wrapper>
+    <Item>
+      <TitleBlock
+        title="For Physicians"
+        subTitle="Our Promise"
+      />
+      <ReadMoreBlock contactUs
+        text={
+          <>
+            <Paragraph>90% First Pass payment within 30 days </Paragraph>
+            <Paragraph>@up to 3% of collected revenue</Paragraph>
+            <Paragraph>Free Credentialing and Re-credentialing</Paragraph>
+          </>
+        }
+        textMore={
+          <Paragraph>Free Credentialing and Re-credentialing</Paragraph>
+        }
+      />
+    </Item>
+    <Item>
+      <TitleBlock
+        title="For Hospitals/Medical Billing Companies"
+        subTitle="Our Promise"
+      />
+      <ReadMoreBlock contactUs
+        text={
+          <>
+            <Paragraph>Increase in revenue by 15%</Paragraph>
+            <Paragraph>Decrease in costs by 40%</Paragraph>
+            <Paragraph>Accelerated cash flows with effective management of denials.</Paragraph>
+          </>
+        }
+        textMore={
+          <Paragraph inline>Accelerated cash flows with effective management of denials.</Paragraph>
+        }
+      />
+    </Item>
+  </Wrapper>
+);
---
+++ src/features/contactUs/Promises/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+
+`;
+
+export const Item = styled.div`
+  margin: 0px 0px 30px 0px;
+`;
+
+export const Paragraph = styled.p`
+  margin: 0px 0px 15px 0px;
+`;

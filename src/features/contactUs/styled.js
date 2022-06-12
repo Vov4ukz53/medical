bf31616be7323ec9ead49cf28b4504b72019c6bd
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-06-12 16:17:09 +0200

    changed margin in section contuct us

--- src/features/contactUs/styled.js
+++ src/features/contactUs/styled.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-  margin: 125px 0px;
+  margin: 125px 0px 0px 0px;
 
   @media(max-width: ${({ theme }) => theme.breakpoints.desktops}){
-    margin: 40px 0px;
+    margin: 40px 0px 0px 0px;
   };
 
   @media(max-width: ${({ theme }) => theme.breakpoints.mobile}){
-    margin: 20px 0px;
+    margin: 20px 0px 0px 0px;
   };
 `;
